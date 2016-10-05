4b35c695644cb36b667466a0cc4c40f751dab375
Ranae Dietzel <ranae.dietzel@gmail.com>
2016-10-05 15:29:14 -0500

    Elaborate on comments and add some "your turn" solutions

--- week_07/dataframes/script.R
+++ week_07/dataframes/script.R
@@ -2,46 +2,39 @@ getwd()
 setwd()
 
 library(readr) 
-crops<-read_csv("NASS-Iowa.csv")  #ctr+enter will run the line you are on 
+crops<-read_csv("NASS-Iowa.csv")  #ctr+enter or command+enter will run the line you are on 
 
-head(crops)  #I like to glance at the data
+head(crops)  #If you want to glance at the data
 tail(crops)  # BTW, hashtag is used to insert commments
 
 #Let's get rid of some columns
 
-  #Choose the column you want to keep
-  less_columns<-crops[,c("Year", "State", "Commodity", "Data.Item", "Value")]
-
-  #Choose columns you don't want to keep
-  fewer_columns<-crops[, -c("Program", "Year", "Period", "Week.Ending", 
-                        "Geo.Level", "State.ANSI", "Ag.Distric")]  
+  #Choose the columns you want to keep
+  fewer_columns<-crops[,c("Year", "State", "Commodity", "Data Item", "Value")]
 
   #Can also specify by column number  
   notsomany_columns<-crops[,c(2, 6, 16 ,17, 20)]
 
-#Let's get rid of some rows - like everything, there are a lot of ways 
+#Let's get rid of some rows - like everything, there are a lot of ways to do the same thing 
   
-  recent<-less_columns[less_columns$Year != 1866, ]
+  recent<-fewer_columns[fewer_columns$Year != 1866, ]
   
-  recent<-less_columns[less_columns$Year %in% c(1867:2015),]  
+  recent<-fewer_columns[fewer_columns$Year %in% c(1867:2015),]  
   
-  recent<-less_columns[1:6819,]  
+  recent<-fewer_columns[1:6819,]  
   
-#Now let's subset soybeans  
-  beans<-recent[recent$Commodity == "SOYBEANS" & recent$Data.Item == "SOYBEANS - YIELD, MEASURED IN BU / ACRE",]  
+#Now let's subset soybeans, just bushels per acre  
+  beans<-recent[recent$Commodity == "SOYBEANS" & 
+          recent$`Data Item` == "SOYBEANS - YIELD, MEASURED IN BU / ACRE",]  
   
-#These columns names are driving me crazy  
+#These columns names are driving me crazy, let's rename  
   colnames(beans)<-c("year", "state", "commodity", "measure", "bu_acre")  
-  beans<-beans[,-4]
+  beans<-beans[,-4] #We don't even need this column, let's remove
   
-#I have some class problems
-  beans$bu_acre<-as.numeric(beans$bu_acre)
-  beans$year<-as.numeric(beans$year)  
-  
-#Add a unit conversion  
+#Add a unit conversion as a new column
   beans$Mg_ha<-beans$bu_acre*60*0.00045*0.40
  
-#Fit a linear model 
+#Fit a linear model to examine the relationship between time and yield
 mod<-lm(beans$bu_acre ~ beans$year)
 
 #Look at the summary
@@ -56,25 +49,67 @@ summary(mod)$coefficients
 #Extract the p-value
 summary(mod)$coefficients[,4] 
 
-#Extract the r squared
+#Extract the R squared
 summary(mod)$r.squared
 
-#Something that was extracted being useful
+#Here is something that was extracted being useful
 library(ggplot2)
 
 ggplot(beans, aes(x=year, y=bu_acre))+
   geom_point()+
   geom_smooth(method='lm')+
-  annotate("text", x=1940, y=50, label = summary(mod)$r.squared, size=5, parse=TRUE)  
+  annotate("text", x=1940, y=50, label = summary(mod)$r.squared, size=5, parse=TRUE)
 
-#More subsetting  
+#######################################################
 
-smlgrains<-less_columns[less_columns$Commodity %in% c("OATS", "BARLEY", "WHEAT", "RYE") & 
-                          less_columns$Data.Item %in% c("OATS - ACRES HARVESTED","BARLEY - ACRES HARVESTED",
+
+
+#######################################################
+#Now we want to look at small grains as a group
+#Subset most of the small grains listed
+smlgrains<-fewer_columns[fewer_columns$Commodity %in% c("OATS", "BARLEY", "WHEAT", "RYE") & 
+                       fewer_columns$`Data Item` %in% c("OATS - ACRES HARVESTED","BARLEY - ACRES HARVESTED",
                                                        "WHEAT - ACRES HARVESTED","RYE - ACRES HARVESTED"),]
 
-smlgrains$Value<-as.numeric(smlgrains$Value)
+#These data from NASS have a comma in them and we have to remove it by
+ #substituting "nothing" in the place of the comma. You will run into random problems like
+ #this a lot and you will always get past it. . .somehow 
+smlgrains$Value<-as.numeric(gsub(",", "", smlgrains$Value))
 
+#Here is a glimpse of your future with an awesome package (dplyr)
+library(dplyr)
 total_smlgrains<-group_by(smlgrains, Year)%>%
   summarise(total=sum(Value))
   
+#Check it out 
+plot(total_smlgrains$total~total_smlgrains$Year)
+
+#####################################################
+#Don't see many small grains these days, but I do see soybeans. I wonder about the 
+ #relationship. Let's compare acre amounts
+
+#Back to beans and this time subset "acres harvested"
+bean_acres<-recent[recent$Commodity == "SOYBEANS" & 
+                recent$`Data Item` == "SOYBEANS - ACRES HARVESTED",] 
+
+#Now let's work to get the "bean_acres" dataframe and "total_smlgrains" dataframe to 
+ #look identical so we tack them together for a tidy result
+bean_acres<-bean_acres[,c(1,5)] #this is the information we actually need
+colnames(bean_acres)<-c("year", "acres")
+
+#Okay, should have left 'Commodity' in there, but now we don't 
+  #have to have SOYBEANS shouted at us
+bean_acres$crop<-"soybeans" 
+
+#Make matching column names and "crop" label   
+colnames(total_smlgrains)<- c("year", "acres")
+total_smlgrains$crop<-"smlgrains"
+
+#Now, R, bind these rows together
+both<-rbind(bean_acres, total_smlgrains)
+
+#Here's a story about cropland use in Iowa
+ggplot(both, aes(x=year, y=acres, group=crop, color=crop))+
+  geom_point()+
+  geom_smooth()+
+  ggtitle("Changes in small grain and soybean acres in Iowa")
