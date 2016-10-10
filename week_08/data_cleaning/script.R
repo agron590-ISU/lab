b4755554b127afd875fadc4719f3f2fcd3484e27
Ranae Dietzel <ranae.dietzel@gmail.com>
2016-10-10 14:43:35 -0500

    Changed filtering parameters

--- week_08/data_cleaning/script.R
+++ week_08/data_cleaning/script.R
@@ -2,20 +2,21 @@
 
 #First, read it in to make it a dataframe with one header
 #Select the line with the most useful information 
-header_weather<-scan("weather.txt")
+header_weather<-scan("weather.txt", skip = 1, sep = "\t", nlines = 1,  what = character())
 #Select all the data
-weather<-read.table("weather.txt")
+weather<-read.table("weather.txt", skip = 4, na.strings = "?", sep = "\t", stringsAsFactors = FALSE)
 #Put the header and the data together
 names(weather)<-header_weather
 
-#Lets just work with time, total rainfall, total solar radiation, max air temp, and min air temp -- select these columns
+#Lets just work with time, total rainfall, total solar radiation, max air temp, 
+  #and min air temp -- select these columns
 
 #Give the column new names -- "date", "total_rain_in", "total_rad_MJ", "max_temp_F", "min_temp_F"
 
 #Filter the data to make sure you have reasonable values
   #Between 0 and 6 for total rain
-  #Between 60 and 100 for total radiation
-  #Between -50 and -10 for both temps
+  #Between 7 and 30 for total radiation
+  #Between -50 and 120 for max temp and -70 and 95 for min temps
   #max temp cannot be lower than min temp
 
 #Create a rain column in millimeters and temperature columns in degrees C
