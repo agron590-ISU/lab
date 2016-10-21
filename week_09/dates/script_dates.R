a2a1f11158368e31f08bbadc082c40b14088d9a8
Ranae Dietzel <ranae.dietzel@gmail.com>
2016-10-21 00:20:02 -0500

    Added some instructions of what to do with the data

--- week_09/dates/script_dates.R
+++ week_09/dates/script_dates.R
@@ -1,10 +1,30 @@
 ###Dates, dplyr, tidyr
 library(tidyverse)
 
-data_dictionary<-data.frame(c("date", "rain", "soil_water", "soy_yield", "maize_yield", "nh4",
+data_dictionary<-data.frame(variable = c("date", "rain", "soil_water", "soil_temp", "soy_yield", "maize_yield", "nh4",
                               "no3", "n_fixation", "n_leaching", "soil_n_min", "n_uptake"), 
-                            c("dd/mm/yyyy", "mm", "mm", "degC", "kg/ha", "kg/ha", "kg/mg",
-                              "kg/mg", ))
-df<-read_csv("future.csv")  
+                            unit = c("dd/mm/yyyy", "mm", "mm", "degC", "kg/ha", "kg/ha", "kg/mg",
+                              "kg/mg", "kg", "kg", "kg", "kg"), 
+                            definition = c("date", "mm of rain", 
+                                            "amount of water in the soil to 15 cm", 
+                                           "soil temp at 15 cm", "soybean grain yield",
+                                           "maize grain yield", "ammonium concentration in soil", 
+                                           "nitrate concentration in soil", "kg of nitrogen fixed", 
+                                           "kg of nitrogen leached", 
+                                            "kg of soil nitrogen produced by mineralization", 
+                                            "kg of nitrogen taken up by the plant" ))
+future<-read_csv("future.csv", na = "?")  
+
+#Use `qplot(x, y, data=future)` to explore a few simple relationships
+#Try `qplot(date, y, data=future) (may take a minute to print)
+
+#Change the dates to the proper format and plot again
+#Add a year column  
+#Add a month column  
+#Add day of year  
+
+#Sum rainfall ammounts for each year and plot across time
+#Find the average soil temp for each year  
+
 
   
